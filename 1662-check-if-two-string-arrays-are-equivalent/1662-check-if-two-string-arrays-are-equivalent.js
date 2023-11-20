f27a121b46245a3b9ff3f4e209939d2ffd0875c6
Ihsan Nafilah Ramdhani <ihsann.ramdhani@gmail.com>
2023-11-20 19:03:23 +0700

    Time: 52 ms (49.23%), Space: 42.3 MB (28.33%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.concat().join('') === word2.concat().join('') ? true : false
+};
