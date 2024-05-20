38a9f0cecf79516545b11db923cf0fb221fab8c3
Ihsan Nafilah Ramdhani <ihsann.ramdhani@gmail.com>
2024-05-20 08:09:36 +0700

    Time: 11 ms (87.32%), Space: 20 MB (42.25%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.php
+++ 2942-find-words-containing-character/2942-find-words-containing-character.php
@@ -8,15 +8,10 @@ class Solution {
     function findWordsContaining($words, $x) {
         $result = array();
         for ($i = 0; $i < count($words); $i++) {
-            $chars = str_split($words[$i]);
-            foreach ($chars as $char) {
-                if ($char == $x) {
-                    array_push($result, $i);
-                    break;
-                }
+            if (str_contains($words[$i], $x)) {
+                array_push($result, $i);
             }
         }
-
         return $result;
     }
 }
